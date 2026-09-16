e8bd25a11f9a11526752da5468cf7b9725dbece3
Hi there! ,Sateesh Meena <59795648+JavaCode2Code@users.noreply.github.com>
2026-09-16 15:53:05 +0530

    Add files via upload

---
+++ K8s-integration-with-spring-boot-pod-communication/KubePodA/src/main/java/org/example/kubepoda/controller/PodAController.java
@@ -0,0 +1,16 @@
+package org.example.kubepoda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PodAController {
+
+	@GetMapping("/getPodA")
+	public String getPodA() {
+		return "Hello from PodA";
+	}
+
+}
